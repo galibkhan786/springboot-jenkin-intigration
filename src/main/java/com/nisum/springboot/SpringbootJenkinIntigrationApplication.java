8c8a051386d631825f2fc1750f4991322566e14e
Galib Shaheed Khan <gkhan@nisum.com>
2022-10-21 00:40:07 +0530

    initial code added

---
+++ src/main/java/com/nisum/springboot/SpringbootJenkinIntigrationApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJenkinIntigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinIntigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisum/springboot/SpringbootJenkinIntigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinIntigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
